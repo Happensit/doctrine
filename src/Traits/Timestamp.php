3f623bcbc7d3e93213449fc51060c3dbb562fe8e
Happensit <antonybizov@google.com>
2017-03-25 00:01:43 +0300

    DeletedAt for Timestamp trait

--- src/Traits/Timestamp.php
+++ src/Traits/Timestamp.php
@@ -20,11 +20,17 @@ trait Timestamp
     private $updatedAt;
 
     /**
+     * @ORM\Column(name="deleted_at", type="datetime", nullable=true)
+     * @var \DateTime
+     */
+    private $deletedAt;
+
+    /**
      * @ORM\PrePersist
      */
     public function prePersist()
     {
-        $now = new Datetime;
+        $now = new DateTime();
         $this->createdAt = $now;
         $this->updatedAt = $now;
     }
@@ -34,27 +40,62 @@ trait Timestamp
      */
     public function preUpdate()
     {
-        $this->updatedAt = new DateTime;
+        $this->updatedAt = new DateTime();
     }
 
+    /**
+     * @return DateTime
+     */
     public function getCreatedAt()
     {
         return $this->createdAt;
     }
 
+    /**
+     * @param DateTime $createdAt
+     */
     public function setCreatedAt(DateTime $createdAt)
     {
         $this->createdAt = $createdAt;
     }
 
+    /**
+     * @return DateTime
+     */
     public function getUpdatedAt()
     {
         return $this->updatedAt;
     }
 
+    /**
+     * @param DateTime $updatedAt
+     */
     public function setUpdatedAt(DateTime $updatedAt)
     {
         $this->updatedAt = $updatedAt;
     }
 
+    /**
+     * @return DateTime
+     */
+    public function getDeletedAt()
+    {
+        return $this->deletedAt;
+    }
+
+    /**
+     * @param DateTime $deletedAt
+     */
+    public function setDeletedAt(DateTime $deletedAt)
+    {
+        $this->deletedAt = $deletedAt;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDeleted()
+    {
+        return (bool) new DateTime() > $this->deletedAt;
+    }
 }
